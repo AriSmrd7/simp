33da6ef3a4496e2b89d401032a58393efd9c1b7b
AriSmrd7 <arismrd13@gmail.com>
2022-12-25 23:11:30 +0700

    update

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
         <footer class="footer">
           <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2018 <a href="https://www.urbanui.com/" target="_blank">Urbanui</a>. All rights reserved.</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2022 <a href="https://www.urbanui.com/" target="_blank">CV Sri Rejeki</a>. All rights reserved.</span>
           </div>
         </footer>
